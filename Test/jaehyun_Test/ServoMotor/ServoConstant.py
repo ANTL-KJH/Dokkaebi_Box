056401d962b7478ece3848a9829086bcbc10a6da
ANTL-KJH <wogus9190@gmail.com>
2023-10-29 11:34:27 +0900

    Update ServoMotor

---
+++ Test/jaehyun_Test/ServoMotor/ServoConstant.py
@@ -0,0 +1,13 @@
+"""
+* Project : 2023 Seoul AIOT Hackathon
+* Program Purpose and Features :
+* - class ServoMotor constant
+* Author : JH KIM
+* First Write Date : 2023.11.03
+* ==========================================================================
+* Program history
+* ==========================================================================
+* Author    		Date		    Version		History
+* JH KIM            2023.11.03		v1.00		First Write
+"""
+SERVO_DEFAULT_FREQ = 50
